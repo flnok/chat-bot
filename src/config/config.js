15f1fe64d0d20655bca72fea73767a7344bdb19f
Canh Nguyen-Huu <nhcanh122@gmail.com>
2022-05-30 13:22:10 +0700

    fix nua

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,6 @@ module.exports = {
   dialogFlowSessionID: uuid.v4(),
   dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_SESSION_LANGUAGE_CODE,
   googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-  googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
+  googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
   defaultLanguageCode: process.env.DEFAULT_LANGUAGE_CODE,
 };
