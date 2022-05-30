54ef7cb48f96e658bf6ecb3e3cbcfb87826d1707
Canh Nguyen-Huu <nhcanh122@gmail.com>
2022-05-30 11:02:29 +0700

    config

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const uuid = require('uuid');
 
-const config = {
+module.exports = {
   googleProjectID: process.env.GOOGLE_PROJECT_ID,
   dialogFlowSessionID: uuid.v4(),
   dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_SESSION_LANGUAGE_CODE,
@@ -8,5 +8,3 @@ const config = {
   // googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
   defaultLanguageCode: process.env.DEFAULT_LANGUAGE_CODE,
 };
-
-module.exports = config;
