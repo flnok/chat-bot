ff2e60258053c6a3ace4a987b2ccf906a1964086
Canh Nguyen-Huu <nhcanh122@gmail.com>
2022-07-15 17:38:36 +0700

    fix

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const connectDB = require('./src/config/database');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const path = require('path');
 
 // Config
 dotenv.config();
