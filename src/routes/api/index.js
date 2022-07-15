758f80899196d545e4e4fa2e81accccb31b63186
Canh Nguyen-Huu <nhcanh122@gmail.com>
2022-07-15 18:17:25 +0700

    1232131

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router(); // /api
 
-// router.get('/', (req, res) => {
-//   return res.send('Đây là trang api');
-// });
+router.get('/', (req, res) => {
+  return res.send('Đây là trang api');
+});
 
 router.use('/dialog-flow', require('./dialogFlow'));
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  return res.send('backend dashboard');
-});
+// router.get('/', (req, res) => {
+//   return res.send('backend dashboard');
+// });
 
 router.use('/api', require('./api'));
 
