ec16933048ec5e7b34df9ffe2ec3d127a62c4fa8
Canh Nguyen-Huu <nhcanh122@gmail.com>
2022-07-15 17:24:59 +0700

    dp5

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheet/index.css';
+import registerServiceWorker from './registerServiceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+registerServiceWorker();
