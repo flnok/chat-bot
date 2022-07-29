148d15c1efc914ade8220ea8e21657e54856c5ab
Canh Nguyen-Huu <nhcanh122@gmail.com>
2022-07-29 13:27:49 +0700

    content

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,19 +1,20 @@
+import data from '../data/content.json';
+
 export default function Contact() {
   return (
     <div className="contact">
-      <h1 className="display-5">Liên hệ</h1>
+      <h1 className="display-5"> {data.contact.content.title}</h1>
       <div>
-        <i className="fa fa-map-marker"></i> 30 Tô Hiến Thành, thị trấn Liên
-        Nghĩa, huyện Đức Trọng, tỉnh Lâm Đồng
+        <i className="fa fa-map-marker"></i> {data.contact.content.address}
       </div>
       <div>
-        <i className="fa fa-envelope-open"></i> abc@gmail.com
-      </div>
-      <div className="text-uppercase">
-        <i className="fa fa-phone"></i> 09 999 789 68
+        <i className="fa fa-envelope-open"></i> {data.contact.content.email}
       </div>
       <div>
-        <i className="fa fa-clock-o"></i> 08:00 - 22:00 (T2-CN)
+        <i className="fa fa-phone"></i> {data.contact.content.phone}
+      </div>
+      <div>
+        <i className="fa fa-clock-o"></i> {data.contact.content.open}
       </div>
       <div className="text-center text-muted noselect p-3">
         © 2022 Copyright: Canh Nguyen-Huu
