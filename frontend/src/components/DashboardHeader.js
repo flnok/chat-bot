724c9fe68594c6cc7674491341795894f1710c57
Canh Nguyen-Huu <nhcanh122@gmail.com>
2022-08-07 20:18:55 +0700

    layout

---
+++ frontend/src/components/DashboardHeader.js
@@ -0,0 +1,60 @@
+import { Container, Nav, Navbar, Row } from 'react-bootstrap';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/auth';
+
+export default function DashboardHeader() {
+  const auth = useAuth();
+  const navigate = useNavigate();
+  const activeStyle = {
+    color: '#8B8C89',
+    textDecoration: 'none',
+  };
+  return (
+    <Navbar expand="lg" sticky="top">
+      <Container fluid className="flex-column">
+        <Row>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="m-auto text-center">
+              <Nav.Link
+                as={NavLink}
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                to="/"
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                as={NavLink}
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                to="/dashboard"
+                end
+              >
+                Dashboard
+              </Nav.Link>
+              <Nav.Link
+                as={NavLink}
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                to="chatbot"
+              >
+                Chatbot
+              </Nav.Link>
+              <Nav.Link
+                as="a"
+                style={{
+                  color: 'teal',
+                  textDecoration: 'none',
+                  cursor: 'pointer',
+                }}
+                onClick={() => {
+                  auth.logout(() => navigate('/'));
+                }}
+              >
+                Logout
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Row>
+      </Container>
+    </Navbar>
+  );
+}
